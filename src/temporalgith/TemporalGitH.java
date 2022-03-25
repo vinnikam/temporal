a6508cccfc9b1e84b9f2b181b72e484e30ca7a84
vinnikam <vinni9@gmail.com>
2022-03-24 23:07:50 -0500

    Version 01 de proyecto

---
+++ src/temporalgith/TemporalGitH.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package temporalgith;
+
+/**
+ *
+ * @author Vinni@
+ */
+public class TemporalGitH {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Conexion a GitHub");
+    }
+    
+}
